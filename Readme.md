7a9d292bd26d61c91393bc540174e7f6ee219ed8
Mark McGranaghan <mmcgrana@gmail.com>
2012-01-06 12:41:16 -0800

    executable platform deploy

--- Readme.md
+++ Readme.md
@@ -17,15 +17,15 @@ $ redis-server
 $ rvm use 1.9.2
 $ bundle install
 $ foreman start
-```bash
+```
 
 
 ## Platform deploy
 
 ```bash
 $ heroku create leech-web-production --stack cedar
-$ heroku addons:add redistogo
-$ heroku config:add ...
+$ heroku addons:add redistogo:medium
+$ heroku config:add DEPLOY=production FORCE_HTTPS=true SESSION_SECRET=`openssl rand -hex 16`
 $ git push heroku master
 $ heroku scale web=2
-```bash
+```
--- lib/leech_web/config.rb
+++ lib/leech_web/config.rb
@@ -2,12 +2,12 @@ ENV["TZ"] = "UTC"
 
 module LeechWeb
   module Config
-    def self.env!(key)
-      ENV[key] || raise("missing #{key}")
+    def self.env!(*keys)
+      keys.map { |k| ENV[k] }.compact.first || raise("missing #{keys.join(",")}")
     end
 
     def self.deploy; env!("DEPLOY"); end
-    def self.redis_url; env!("REDIS_URL"); end
+    def self.redis_url; env!("REDIS_URL", "DEPLOYMASTER_REDIS_URL"); end
     def self.session_secret; env!("SESSION_SECRET"); end
     def self.port; env!("PORT"); end
     def self.force_https?; env!("FORCE_HTTPS") == "true"; end
