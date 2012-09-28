a60014559c77f091bde8b7690d9679e87996a6fb
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-28 13:12:28 -0500

    more web specs

--- spec/umpire/web_spec.rb
+++ spec/umpire/web_spec.rb
@@ -16,7 +16,7 @@ module Umpire
     describe "GET /check" do
       context "without basic auth" do
         it "should require basic auth" do
-          get '/check'
+          get "/check"
           last_response.status.should eq(401)
         end
       end
@@ -27,8 +27,27 @@ module Umpire
         end
         
         it "should return a 400 if params are not passed" do
-          get '/check'
+          get "/check"
           last_response.status.should eq(400)
+        end
+
+        it "should call Graphite.get_values_for_range" do
+          mock_graphite_url = "https://graphite.example.com"
+          Umpire::Config.stub(:graphite_url) { mock_graphite_url }
+          Umpire::Graphite.should_receive(:get_values_for_range).with(mock_graphite_url, "foo.bar", 60) { [] }
+          get "/check?metric=foo.bar&range=60&max=100"
+        end
+
+        it "should return a 404 if there are no data points" do
+          Graphite.stub(:get_values_for_range) { [] }
+          get "/check?metric=foo.bar&range=60&max=100"
+          last_response.status.should eq(404)
+        end
+
+        it "should return a 200 if there are no data points and empty_ok is passed" do
+          Graphite.stub(:get_values_for_range) { [] }
+          get "/check?metric=foo.bar&range=60&max=100&empty_ok=true"
+          last_response.status.should eq(200)
         end
       end
     end
