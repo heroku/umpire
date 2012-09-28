8c74940a8295805f399de7dc59096c0133083f0d
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-28 12:03:25 -0500

    spec cleanup + ensuring that we handle empty data from graphite safely

--- lib/umpire/graphite.rb
+++ lib/umpire/graphite.rb
@@ -5,7 +5,8 @@ module Umpire
     def get_values_for_range(graphite_url, metric, range)
       json = RestClient.get("#{graphite_url}/render/?target=#{metric}&format=json&from=-#{range}s")
       data = JSON.parse(json)
-      data.first["datapoints"].map { |v, _| v }.compact
+
+      data.empty? ? [] : data.first["datapoints"].map { |v, _| v }.compact
     end
   end
 end
--- spec/umpire/graphite_spec.rb
+++ spec/umpire/graphite_spec.rb
@@ -5,22 +5,28 @@ describe Umpire::Graphite do
     let(:graphite_url) { "https://graphite.example.com" }
     let(:metric) { "foo.bar" }
     let(:range) { 60 }
-
-    before(:each) do
-      @stub = stub_request(:get, "#{graphite_url}/render/?format=json&from=-#{range}s&target=#{metric}").to_return(:body => [{"target"=>metric, "datapoints"=>[[4.47, 1348851060]]}].to_json)
-    end
+    let(:stub_request_with_values) { stub_request(:get, "#{graphite_url}/render/?format=json&from=-#{range}s&target=#{metric}").to_return(:body => [{"target"=>metric, "datapoints"=>[[4.47, 1348851060]]}].to_json) }
+    let(:stub_request_without_values) { stub_request(:get, "#{graphite_url}/render/?format=json&from=-#{range}s&target=#{metric}").to_return(:body => [].to_json) }
 
     it "should return an Array" do
+      stub_request_with_values
       Umpire::Graphite.get_values_for_range(graphite_url, metric, range).should be_kind_of(Array)
     end
 
     it "should make a call out to graphite" do
+      stub_request_with_values
       Umpire::Graphite.get_values_for_range(graphite_url, metric, range)
-      @stub.should have_been_requested.once
+      stub_request_with_values.should have_been_requested.once
     end
 
     it "should return the expected values" do
+      stub_request_with_values
       Umpire::Graphite.get_values_for_range(graphite_url, metric, range).should eq([4.47])
+    end
+
+    it "should return an empty array if graphite returns empty data" do
+      stub_request_without_values
+      Umpire::Graphite.get_values_for_range(graphite_url, metric, range).should eq([])
     end
   end
 end
