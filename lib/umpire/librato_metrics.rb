a2f4a487fcc8e1a55f7c997f1060672045b1c291
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-10-23 09:40:01 -0500

    more concise

--- lib/umpire/librato_metrics.rb
+++ lib/umpire/librato_metrics.rb
@@ -6,11 +6,7 @@ module Umpire
       begin
         start_time = Time.now.to_i - range
         results = client.fetch(metric, :start_time => start_time, :summarize_sources => true)
-        if results.has_key?('all')
-          results["all"].map { |h| h["value"] }
-        else
-          []
-        end
+        results.has_key?('all') ? results["all"].map { |h| h["value"] } : []
       rescue Librato::Metrics::NotFound
         raise MetricNotFound
       rescue Librato::Metrics::NetworkError
