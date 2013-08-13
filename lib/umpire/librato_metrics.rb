e675a7b68e5cc291c8b1845596ec858bf1426b6d
Jannis Hermanns <jannis@gmail.com>
2013-08-13 17:56:16 +0200

    Revert "More debugging output"
    This reverts commit 5edc4c45c5dbb636e23db25c8a51029552f70ba3.

--- lib/umpire/librato_metrics.rb
+++ lib/umpire/librato_metrics.rb
@@ -58,7 +58,7 @@ module Umpire
       metric = metric.split(":").first
 
       if Config.debug?
-        Log.log(options.merge(range: range, metric: metric, from: from))
+        Log.log(options.merge(range: range, metric: metric))
       end
 
       results = client.fetch(metric, options)
@@ -84,11 +84,6 @@ module Umpire
 
       composite = CompositeMetric.for(function)
       values = metrics.map { |m| get_values_for_range(m, range, options) }
-
-      if Config.debug?
-        Log.log(values)
-      end
-
       composite.new(*values).value
     end
 
