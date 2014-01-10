e0bce6c9f29df7cad467ee321814fc528f7ab0b2
Ricardo Chimal, Jr <ricardo@heroku.com>
2014-01-10 00:43:54 -0800

    Umpire::Log: go silent when testing

--- lib/umpire/log.rb
+++ lib/umpire/log.rb
@@ -4,7 +4,11 @@ require "scrolls"
 module Umpire
   module Log
     def self.log(data, &blk)
-      Scrolls.log({app: "umpire", deploy: Config.deploy}.merge(data), &blk)
+      if Config.deploy == "test"
+        blk.call if blk
+      else
+        Scrolls.log({app: "umpire", deploy: Config.deploy}.merge(data), &blk)
+      end
     end
   end
 end
